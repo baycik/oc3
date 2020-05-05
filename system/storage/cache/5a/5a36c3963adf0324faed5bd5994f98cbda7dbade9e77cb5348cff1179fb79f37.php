18e6752a2137d34c86ae9752cdabdf32546f11a2
bayci <bayci@BaycikNoteL.Dlink>
2020-05-05 11:41:15 +0300

    Initial commit

---
+++ system/storage/cache/5a/5a36c3963adf0324faed5bd5994f98cbda7dbade9e77cb5348cff1179fb79f37.php
@@ -0,0 +1,45 @@
+<?php
+
+/* common/footer.twig */
+class __TwigTemplate_39eba91a40ab4ec6c3c621010eb6dff408d66c14492db7c3e1cb2ff997374cf2 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<footer id=\"footer\">";
+        echo (isset($context["text_footer"]) ? $context["text_footer"] : null);
+        echo "<br />";
+        echo (isset($context["text_version"]) ? $context["text_version"] : null);
+        echo "</footer></div>
+</body></html>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "common/footer.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
+/* <footer id="footer">{{ text_footer }}<br />{{ text_version }}</footer></div>*/
+/* </body></html>*/
+/* */
