e4bb6dc7a3dfd26ee00ea6b8e78d427bedd7a28b
bayci <bayci@BaycikNoteL.Dlink>
2021-06-29 16:55:17 +0300

    bugfix

--- Modificators/iSellSoft-SearchAndFilter.ocmod/upload/admin/language/en-gb/extension/module/iss_filter.php
+++ Modificators/iSellSoft-SearchAndFilter.ocmod/upload/admin/language/en-gb/extension/module/iss_filter.php
@@ -1,20 +1,20 @@
 <?php
 // Heading
-$_['heading_title']       = 'Improved Search Engine';
+$_['heading_title']       = 'Super Filter';
 
 // Text
 $_['text_extension']      = 'Extensions';
-$_['text_success']        = 'Settings are updated successfully!';
-$_['text_edit']           = 'Edit module';
+$_['text_success']        = 'Settings are saved successfully!';
+$_['text_edit']           = 'Module settings';
 
 // Entry
 $_['entry_status']        = 'Status';
 
 // Error
-$_['error_permission']    = 'You have no permission to edit this module!';
+$_['error_permission']    = 'You have no permission to change this extension!';
 
-$_['install_keys']        = 'Install keys';
-$_['uninstall_keys']      = 'Delete keys';
-$_['keys_installed']      = 'Keys are installed';
-$_['keys_uninstalled']    = 'Installation failed! Search engine will not work properly!';
-$_['keys_error']          = 'Error occured while installing/deleting keys';
+$_['install_keys']        = 'Установить ключи';
+$_['uninstall_keys']      = 'Удалить ключи';
+$_['keys_installed']      = 'Ключи установлены в таблицы';
+$_['keys_uninstalled']    = 'Ключи неустановлены! Поиск работать не будет!';
+$_['keys_error']          = 'Ошибка установки/удаления ключей';
